891447a2575bf0f5eb99a23af051f016a26b61a7
Justas-Sind <justas.sindar@gmail.com>
2023-03-10 14:04:13 +0200

    Minor style changes, filled works section

--- src/components/WorksPage/WorksPage.tsx
+++ src/components/WorksPage/WorksPage.tsx
@@ -1,7 +1,15 @@
 import styles from "./WorksPage.module.scss";
 import { motion } from "framer-motion";
+import developmentImage from "../../assets/images/undraw_building_websites_i78t.svg";
+import PageButton from "../PageButton/PageButton";
+import { useNavigate } from "react-router-dom";
 
 function WorksPage() {
+  const navigation = useNavigate();
+
+  function handleOnClick() {
+    navigation("/contact");
+  }
   return (
     <motion.div
       className={styles.worksPage}
@@ -15,7 +23,13 @@ function WorksPage() {
         </div>
         <div className={styles.worksContent}>
           <div className={styles.worksGridContainer}>
-
+            <h3>Work in progress...</h3>
+            <PageButton handleOnClick={handleOnClick}>Contact</PageButton>
+          </div>
+          <div className={styles.imageContent}>
+            <div className={styles.imgContainer}>
+              <img src={developmentImage} alt="development picture" draggable="false" />
+            </div>
           </div>
         </div>
       </div>
