191bde44b5e5f4cb76eff5efe88b5850f4debb54
Justas-Sind <justas.sindar@gmail.com>
2023-03-10 14:15:13 +0200

    404 page added

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,28 @@
-import styles from './NotFoundPage.module.scss';
+import styles from "./NotFoundPage.module.scss";
+import { motion } from "framer-motion";
+import notFoundImage from "../../assets/images/undraw_page_not_found_re_e9o6.svg";
 
 function NotFoundPage() {
   return (
-    <>
-    </>
+    <motion.div
+      className={styles.notFoundPage}
+      initial={{ translateX: "-100%", opacity: 0 }}
+      animate={{ translateX: 0, opacity: 1, transition: { duration: 0.75 } }}
+      exit={{ translateX: "110%", opacity: 0, transition: { duration: 0.25 } }}
+    >
+      <div className={styles.notFoundPageContainer}>
+        <div className={styles.notFoundPageContent}>
+          <div className={styles.imgContainer}>
+            <img
+              src={notFoundImage}
+              alt="404 page not found picture"
+              draggable="false"
+            />
+          </div>
+          <h2>Page not found</h2>
+        </div>
+      </div>
+    </motion.div>
   );
 }
 
